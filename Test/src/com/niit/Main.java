262ea9bf580b83425a8252606f83572be6f2c202
pramilag23 <83206913+pramilag23@users.noreply.github.com>
2021-05-03 13:56:33 +0530

    "Changes in Main.java"

--- Test/src/com/niit/Main.java
+++ Test/src/com/niit/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Program");
+		System.out.println("Test Program1");
 
 	}
 
